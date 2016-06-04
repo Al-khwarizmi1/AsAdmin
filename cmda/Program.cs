6fc24fe895ad28c56cb9b1a2259c6e6e28fe8555
Floatas <floatas@outlook.com>
2016-06-04 20:01:56 +0300

    initial

---
+++ cmda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace cmda
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    FileName = @"C:\Windows\system32\cmd.exe",
+                    UseShellExecute = true,
+                    Verb = "runas",
+                    Arguments = $"/s /k pushd \"{Directory.GetCurrentDirectory()}\" "
+                }
+            };
+
+            p.Start();
+        }
+    }
+}
---
+++ psa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace psa
+{
+    class Program
+    {
+        static void Main()
+        {
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    FileName = @"powershell.exe",
+                    UseShellExecute = true,
+                    Verb = "runas",
+                    Arguments = $"-noexit -command \"cd '{Directory.GetCurrentDirectory()}'\" "
+                }
+            };
+
+            p.Start();
+        }
+    }
+}
